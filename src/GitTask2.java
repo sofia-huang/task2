042364f14a0f5692f6bbd8dda9db248fafe5b884
Sofia Huang <HuangSF20@pwcs-edu.org>
2019-05-21 12:05:05 -0400

    java

--- src/GitTask2.java
+++ src/GitTask2.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class GitTask2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Give two numbers, smallest first");
+		int min = input.nextInt();
+		int max = input.next();
+		System.out.println((int)(Math.random()*max));
 
 	}
 
